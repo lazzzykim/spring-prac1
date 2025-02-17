2baeabf12a375510e8dfea712124daeb50228950
lazzzykim <33sunghyeon@gmail.com>
2025-02-18 00:16:45 +0900

    JPA, AOP 구현

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,16 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
 
     public long getId() {
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -17,9 +19,7 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
-
     public Long join(Member member) {
-        // 중복 회원
         validateDuplicatedMember(member);
         memberRepository.save(member);
         return member.getId();
